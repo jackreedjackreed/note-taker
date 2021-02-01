474d5feffc5d0fd1268a629cc718a59dfadc9d66
J-Boogie <jackr353@gmail.com>
2021-02-01 10:38:35 -0600

    try adding nodemon but not working, add route to index.html from server.js

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -7,16 +7,16 @@ module.exports = function(app) {
     // with get, there are two params: app/url that we're gonna hit, a function that gets request/response --> logic to give front end proper html file
     // __dirname represents the absolute path --> built in "get" for the absolute path
     app.get("/", function(req, res) {
-        res.sendFile(path.join(__dirname, "../public/assets/home.html"));
+        res.sendFile(path.join(__dirname, "../public/home.html"));
     });
 
     app.get("/notes", function(req, res) {
-        res.sendFile(path.join(__dirname, "../public/assets/notes.html"));
+        res.sendFile(path.join(__dirname, "../public/notes.html"));
     });
 
      // If no matching route is found default to home
     app.get("*", function(req, res) {
-        res.sendFile(path.join(__dirname, "../public/assets/home.html"));
-        
+        res.sendFile(path.join(__dirname, "../public/home.html"));
+
      });
 }
--- server.js
+++ server.js
@@ -10,6 +10,22 @@ var PORT = process.env.PORT || 3030
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 
+var yoda = {
+    name: "Yoda",
+    role: "Jedi Master",
+    age: 900,
+    forcePoints: 2000
+  };
+
+app.get("/yoda", function(req, res) {
+    res.json(yoda);
+  });
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
+
 // start the server
 app.listen(PORT, function() {
     console.log("app listening on PORT: " + PORT);
