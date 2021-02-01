8593feff72bd8532904033d9fc3a424654006a1f
J-Boogie <jackr353@gmail.com>
2021-02-01 16:44:01 -0600

    unsuccessful attempts at api get and post routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,39 +1,62 @@
 const { response } = require("express");
+// pull in data from db
+var fs = require("fs");
+var db = require("../db/db.json");
+const { v4: uuidv4 } = require("uuid");
 
 var yoda = {
-    name: "Yoda",
-    role: "Jedi Master",
-    age: 900,
-    forcePoints: 2000
-  };
+  name: "Yoda",
+  role: "Jedi Master",
+  age: 900,
+  forcePoints: 2000,
+};
 
+console.log(db);
 
-module.exports = function(app) {
+module.exports = function (app) {
 
-    app.get("/api/notes", function(request, response) {
-        return response.json(notes);
+    // API GET REQUEST
+    app.get("/api/notes", function (request, response) {
+    fs.readFile(
+      db,
+      { encoding: "utf8", flag: "r" },
+      function (err, data) {
+        if (err) console.log(err);
+        else console.log(data);
+      }
+    );
+    console.log("getting api/notes");
+    response.json(db);
+  });
+
+    // API POST REQUEST
+  app.post("/api/notes", function (request, response) {
+    // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
+    // It will do this by sending out the value "true" have a table
+    // req.body is available since we're using the body parsing middleware
+    fs.readFile(data, function (err, data) {
+        if (err) throw err;
+        var notesArray = JSON.parse(data)
+        var newNote = {...request.body, id: uuid() };         // code source @jongomezdev
+        notesArray.push(newNote);
+        fs.writeFileSync(db, JSON.stringify(notesArray));
+        response.json(newNote);
     })
+  })
+};
 
-    app.get("/api/yoda", function(req, res) {
-        return res.json(yoda);
-      });
 
-    // app.get("/api/waitlist", function(request, response){
-    //     return response.json(waitlist)
-    // })
+  // ---------------------------------------------------------------------------
+  // I added this below code so you could clear out the table while working with the functionality.
+  // Don"t worry about it!
 
-    // app.post("/api/tables", function(request, response) {
-    //     var data = request.body;
-    //     console.log(data);
-    //     if (tableData.length < 5) {
-    //         tableData.push(data);
-    //         response.json("Bingo!");
-    //     } else {
-    //         waitlist.push(data);
-    //         response.json("Oop u just got added to the waitlist")
-    //     }
-    // })    
-}
+//   app.post("/api/clear", function (req, res) {
+//     // Empty out the arrays of data
+//     tableData.length = 0;
+//     waitListData.length = 0;
+
+//     res.json({ ok: true });
+//   });
 
 // Reservation List
 
