8568d751cd2aeeed451ba24b6df32303defe852e
J-Boogie <jackr353@gmail.com>
2021-02-01 09:40:58 -0600

    add html routes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -7,14 +7,16 @@ module.exports = function(app) {
     // with get, there are two params: app/url that we're gonna hit, a function that gets request/response --> logic to give front end proper html file
     // __dirname represents the absolute path --> built in "get" for the absolute path
     app.get("/", function(req, res) {
-        res.sendFile(path.join(__dirname, "../public/home.html"));
+        res.sendFile(path.join(__dirname, "../public/assets/home.html"));
     });
 
-    app.get("/reserve", function(req, res) {
-        res.sendFile(path.join(__dirname, "../public/reserve.html"));
+    app.get("/notes", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/assets/notes.html"));
     });
 
-    app.get("/tables", function(req, res) {
-        res.sendFile(path.join(__dirname, "../public/tables.html"));
+     // If no matching route is found default to home
+    app.get("*", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/assets/home.html"));
+        
      });
 }
